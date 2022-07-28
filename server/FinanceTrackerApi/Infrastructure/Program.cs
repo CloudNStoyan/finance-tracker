e96a2d7e44ecb0f76f4c38dd15b1bf1fda48a1fc
Stoyan Kolev <slownessbg@gmail.com>
2022-07-28 20:37:07 +0300

    fixing cors errors

--- server/FinanceTrackerApi/Infrastructure/Program.cs
+++ server/FinanceTrackerApi/Infrastructure/Program.cs
@@ -7,12 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddHttpContextAccessor();
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
 
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 builder.Host.ConfigureContainer<ContainerBuilder>(autofacBuilder =>
     autofacBuilder.RegisterModule(new MainModule(builder.Configuration)));
 
 var app = builder.Build();
+
+app.UseCors("corsapp");
 
 app.UseAuthorization();
 
