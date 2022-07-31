b1a8ab25583930def24a3a552a2bc1c31cade782
Stoyan Kolev <slownessbg@gmail.com>
2022-08-01 02:11:20 +0300

    fixing typo

--- client/src/pages/styles/RegisterPage.styled.tsx
+++ client/src/pages/styles/RegisterPage.styled.tsx
@@ -18,7 +18,7 @@ const RegisterPageStyled = styled.div`
     background: url(${({ theme }) => theme.svgs.bottomWavesFlipped});
     background-repeat: no-repeat;
     background-size: cover;
-    background-position-y: 25px
+    background-position-y: 25px;
     z-index: -1;
   }
 
