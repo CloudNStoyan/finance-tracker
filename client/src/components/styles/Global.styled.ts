17827792f5be5a4335e11777c9b54abab9b751ac
Stoyan Kolev <slownessbg@gmail.com>
2022-07-31 18:45:01 +0300

    fixing height of the layout and moving imports to normal css file

--- client/src/components/styles/Global.styled.ts
+++ client/src/components/styles/Global.styled.ts
@@ -2,13 +2,18 @@ import { createGlobalStyle } from "styled-components";
 import { StyledTheme } from "../../infrastructure/ThemeManager";
 
 const GlobalStyles = createGlobalStyle<{ theme: StyledTheme }>`
-  @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
-  @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
-
-  body, #app {
-    min-height: 100vh;
+  html {
+  height: -webkit-fill-available;
+  }
+  body {
+    min-height: -webkit-fill-available;
+    display: flex;
     background-color: ${({ theme }) => theme.colors.background};
     color: ${({ theme }) => theme.colors.text};
+  }
+
+  #app {
+    flex-grow: 1;
   }
 `;
 
--- client/src/components/styles/ThemesComponent.styled.ts
+++ client/src/components/styles/ThemesComponent.styled.ts
@@ -2,6 +2,7 @@ import { styled } from "../../infrastructure/ThemeManager";
 
 const ThemesComponentStyled = styled.div`
   padding-top: 50px;
+  height: 100%;
 `;
 
 export default ThemesComponentStyled;
