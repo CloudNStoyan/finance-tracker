441377fd0a4d818a7ed974d352f0b2cd72e1edbc
Stoyan Kolev <slownessbg@gmail.com>
2022-08-18 05:38:43 +0300

    fixing bug

--- client/src/components/CalendarTransactionList.tsx
+++ client/src/components/CalendarTransactionList.tsx
@@ -21,36 +21,37 @@ const CalendarTransactionList = () => {
 
   return (
     <div className="flex flex-col gap-1 p-1 h-full mt-2 w-screen mb-2 overflow-hidden overflow-y-scroll">
-      {transactions
-        .filter((transaction) => {
-          const date = fromUnixTimeMs(selected);
-          const transactionDate = parseJSON(transaction.transactionDate);
-          if (
-            (isAfter(date, transactionDate) &&
-              transaction.repeat === "weekly" &&
-              transactionDate.getDay() === date.getDay()) ||
-            (transaction.repeat === "monthly" &&
-              transactionDate.getDate() === date.getDate()) ||
-            (transaction.repeat === "yearly" &&
-              transactionDate.getDate() === date.getDate() &&
-              transactionDate.getMonth() === date.getMonth())
-          ) {
-            return true;
-          }
-
-          return DatesAreEqualWithoutTime(transactionDate, date);
-        })
-        .map((transaction, idx) => (
-          <TransactionInline
-            transaction={transaction}
-            category={
-              categories.find(
-                (cat) => cat.categoryId === transaction.categoryId
-              ) ?? DefaultCategory
+      {selected &&
+        transactions
+          .filter((transaction) => {
+            const date = fromUnixTimeMs(selected);
+            const transactionDate = parseJSON(transaction.transactionDate);
+            if (
+              (isAfter(date, transactionDate) &&
+                transaction.repeat === "weekly" &&
+                transactionDate.getDay() === date.getDay()) ||
+              (transaction.repeat === "monthly" &&
+                transactionDate.getDate() === date.getDate()) ||
+              (transaction.repeat === "yearly" &&
+                transactionDate.getDate() === date.getDate() &&
+                transactionDate.getMonth() === date.getMonth())
+            ) {
+              return true;
             }
-            key={idx}
-          />
-        ))}
+
+            return DatesAreEqualWithoutTime(transactionDate, date);
+          })
+          .map((transaction, idx) => (
+            <TransactionInline
+              transaction={transaction}
+              category={
+                categories.find(
+                  (cat) => cat.categoryId === transaction.categoryId
+                ) ?? DefaultCategory
+              }
+              key={idx}
+            />
+          ))}
     </div>
   );
 };
