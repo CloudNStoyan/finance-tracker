0896ab30ecd12c1eb780677643f493fa72f32025
Stoyan Kolev <slownessbg@gmail.com>
2022-09-01 23:21:14 +0300

    moving colors to own file

--- client/src/pages/CategoryPage.tsx
+++ client/src/pages/CategoryPage.tsx
@@ -16,55 +16,9 @@ import {
   deleteCategory,
 } from "../state/categorySlice";
 import DeleteDialog from "../components/DeleteDialog";
+import { COLORS, INITIAL_COLOR_INDEX } from "../infrastructure/CategoryColors";
 
 export type UseParamsCategoryType = { categoryId: number };
-
-const INITIAL_COLOR_INDEX = 20;
-
-const COLORS = [
-  "rgb(243, 208, 79)",
-  "rgb(249, 196, 99)",
-  "rgb(243, 156, 18)",
-  "rgb(221, 138, 6)",
-  "rgb(211, 84, 0)",
-  "rgb(178, 73, 2)",
-  "rgb(166, 52, 20)",
-  "rgb(244, 98, 98)",
-  "rgb(218, 64, 64)",
-  "rgb(195, 25, 58)",
-  "rgb(112, 11, 30)",
-  "rgb(242, 136, 177)",
-  "rgb(211, 77, 129)",
-  "rgb(189, 102, 168)",
-  "rgb(163, 61, 138)",
-  "rgb(115, 89, 182)",
-  "rgb(94, 74, 146)",
-  "rgb(102, 135, 231)",
-  "rgb(57, 96, 209)",
-  "rgb(82, 188, 232)",
-  "rgb(22, 157, 213)",
-  "rgb(0, 112, 177)",
-  "rgb(86, 205, 208)",
-  "rgb(0, 158, 162)",
-  "rgb(87, 218, 187)",
-  "rgb(25, 188, 150)",
-  "rgb(50, 157, 105)",
-  "rgb(41, 117, 65)",
-  "rgb(39, 174, 96)",
-  "rgb(91, 201, 83)",
-  "rgb(168, 200, 91)",
-  "rgb(173, 183, 73)",
-  "rgb(156, 166, 110)",
-  "rgb(155, 136, 43)",
-  "rgb(116, 80, 21)",
-  "rgb(144, 93, 101)",
-  "rgb(177, 153, 147)",
-  "rgb(168, 192, 168)",
-  "rgb(102, 134, 134)",
-  "rgb(176, 182, 186)",
-  "rgb(97, 109, 117)",
-  "rgb(57, 72, 82)",
-];
 
 const CustomTextField = styled(TextField)({
   "& .MuiInputBase-input": {
