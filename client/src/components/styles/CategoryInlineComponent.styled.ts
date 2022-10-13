32fc4b1049cd77961551df3f29c142fc7afcb2d1
Stoyan Kolev <slownessbg@gmail.com>
2022-10-13 17:45:09 +0300

    Adding animations to manage categories page

--- client/src/components/styles/CategoryInlineComponent.styled.ts
+++ client/src/components/styles/CategoryInlineComponent.styled.ts
@@ -6,6 +6,7 @@ type Props = {
 
 const CategoryInlineComponentStyled = styled.div<Props>`
   background-color: ${({ bgColor }) => bgColor};
+  animation: custom-slide-bottom-up 0.25s;
 `;
 
 export default CategoryInlineComponentStyled;
