d6e64f59793fd17d1b54c06589d5321a5521ee8d
Stoyan Kolev <slownessbg@gmail.com>
2023-01-15 23:02:14 +0200

    adding the base landing page

--- client/src/infrastructure/MobileAuthRoutes.tsx
+++ client/src/infrastructure/MobileAuthRoutes.tsx
@@ -1,10 +1,12 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import LandingPage from "../pages/LandingPage";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
 
 const MobileAuthRoutes = () => {
   return (
     <Routes>
+      <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="*" element={<Navigate to="/login" />} />
---
+++ client/src/pages/styles/LandingPage.styled.ts
@@ -0,0 +1,24 @@
+import { styled } from "../../infrastructure/ThemeManager";
+
+const LandingPageStyled = styled.div`
+  margin: 10px;
+
+  p {
+    text-align: justify;
+  }
+
+  h1 {
+    font-size: 2rem;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+  }
+
+  h1,
+  h2 {
+    text-align: center;
+  }
+`;
+
+export default LandingPageStyled;
