02dc1e193bf493a17b021b72516891830491954a
Stoyan Kolev <slownessbg@gmail.com>
2023-01-16 23:41:37 +0200

    adding the base for desktop landing page

--- client/src/infrastructure/DesktopAuthRoutes.tsx
+++ client/src/infrastructure/DesktopAuthRoutes.tsx
@@ -1,10 +1,12 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import DesktopLandingPage from "../pages/desktop/DesktopLandingPage";
 import DesktopLoginPage from "../pages/desktop/DesktopLoginPage";
 import DesktopRegisterPage from "../pages/desktop/DesktopRegisterPage";
 
 const DesktopAuthRoutes = () => {
   return (
     <Routes>
+      <Route path="/" element={<DesktopLandingPage />} />
       <Route path="/login" element={<DesktopLoginPage />} />
       <Route path="/register" element={<DesktopRegisterPage />} />
       <Route path="*" element={<Navigate to="/login" />} />
---
+++ client/src/pages/desktop/DesktopLandingPage.tsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import DesktopLandingPageStyled from "../styles/desktop/DesktopLandingPage.styled";
+
+const DesktopLandingPage = () => {
+  return (
+    <DesktopLandingPageStyled>
+      <h1>Finance Tracker</h1>
+      <h2>Managing money, made simple</h2>
+      <p>
+        Calendar based personal finance management designed for real life.
+        <br />
+        Add past, future or recurring transactions and categorize them. Track
+        them on mobile and plan ahead on the web, or just mix things up.
+      </p>
+      <p>
+        See where your money goes and how your balance evolves over time
+        <br />
+        Be smarter about money. With your balance calculated automatically for
+        each day, month or year, understanding your current and projected
+        financial situation is so easy you might even call it fun.
+      </p>
+      <section>
+        <h2>What our users say about us</h2>
+      </section>
+      <section>
+        <h1>Start your journey towards personal financial freedom.</h1>
+        <Link to={"/register"}>Sign up now</Link>
+      </section>
+    </DesktopLandingPageStyled>
+  );
+};
+
+export default DesktopLandingPage;
