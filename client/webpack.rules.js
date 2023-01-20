297d952933a9519a2864b73bd71b0a0d89849711
Stoyan Kolev <slownessbg@gmail.com>
2023-01-20 09:10:20 +0200

    importing babelconfig to comply with DRY

--- client/webpack.rules.js
+++ client/webpack.rules.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const babelConfig = require("./babel.config.js");
 
 module.exports = [
   {
@@ -7,11 +8,7 @@ module.exports = [
     use: {
       loader: "babel-loader",
       options: {
-        presets: [
-          "@babel/preset-env",
-          ["@babel/preset-react", { runtime: "automatic" }],
-          "@babel/preset-typescript",
-        ],
+        presets: babelConfig.presets,
         plugins: [
           [
             "babel-plugin-direct-import",
